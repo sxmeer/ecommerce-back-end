38374942267c838ef23026bd935a21336d36b42f
sxmeer <sameeryadav2421@gmail.com>
2021-08-25 01:42:23 +0530

    file upload poc file added in the controller

---
+++ firebase.js
@@ -0,0 +1,16 @@
+const firebase = require("firebase")
+const firebaseApp = firebase.initializeApp({
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+});
+
+const storage = firebase.storage();
+
+module.exports = {
+  storage
+}

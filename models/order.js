308fd090b4c188de19c589cc6d2ef8209ac67606
sxmeer <sameeryadav2421@gmail.com>
2021-08-27 01:32:21 +0530

    added create order for COD; edit order, list order APIs done: testing pending

--- models/order.js
+++ models/order.js
@@ -1,59 +1,83 @@
 const mongoose = require('mongoose');
 const { ObjectId } = mongoose.Schema;
 
+const ORDER_PAGINATION_CONFIG = {
+  DEFAULT_PAGE: 1,
+  DEFAULT_LIMIT: 5
+};
+
 const ORDER_STATUS = {
   STATUS_NEW: "STATUS_NEW",
   STATUS_PROCESSING: "STATUS_PROCESSING",
   STATUS_SHIPPED: "STATUS_SHIPPED",
   STATUS_DELIVERED: "STATUS_DELIVERED"
 };
+const ORDER_STATUS_VALUES = Object.values(ORDER_STATUS);
+
 const PAYMENT_STATUS = {
   STATUS_NOT_PAID: "STATUS_NOT_PAID",
   STATUS_PAID: "STATUS_PAID"
 };
+const PAYMENT_STATUS_VALUES = Object.values(PAYMENT_STATUS);
+
 const PAYMENT_METHOD = {
   METHOD_COD: "METHOD_COD",
   METHOD_ONLINE_PAYMENT: "METHOD_ONLINE_PAYMENT"
 };
 
+const PAYMENT_METHOD_VALUES = Object.values(PAYMENT_METHOD);
+
 const orderSchema = new mongoose.Schema({
   products: {
     type: Array,
-    default: []
+    default: [],
+    required: true
   },
   transactionId: {
     type: String
   },
   totalPrice: {
-    type: Number
+    type: Number,
+    required: true
   },
   address: {
     type: String,
     trim: true,
+    required: true,
+    min: [10, "address should be min 10 characters"],
+    max: [50, "address can be maximum 50 characters"]
   },
   orderStatus: {
     type: String,
     default: ORDER_STATUS.STATUS_NEW,
-    enum: [
-      ORDER_STATUS.STATUS_NEW,
-      ORDER_STATUS.STATUS_PROCESSING,
-      ORDER_STATUS.STATUS_SHIPPED,
-      ORDER_STATUS.STATUS_DELIVERED
-    ]
+    enum: {
+      values: ORDER_STATUS_VALUES,
+      message: `values other than ${ORDER_STATUS_VALUES.join(", ")} not supported`
+    },
+    required: true
   },
   paymentStatus: {
     type: String,
     default: PAYMENT_STATUS.STATUS_NOT_PAID,
-    enum: [PAYMENT_STATUS.STATUS_NOT_PAID, PAYMENT_STATUS.STATUS_PAID]
+    enum: {
+      values: PAYMENT_STATUS_VALUES,
+      message: `values other than ${PAYMENT_STATUS_VALUES.join(", ")} not supported`
+    },
+    required: true
   },
   paymentMethod: {
     type: String,
     default: PAYMENT_METHOD.METHOD_COD,
-    enum: [PAYMENT_METHOD.METHOD_COD, PAYMENT_METHOD.METHOD_ONLINE_PAYMENT]
+    enum: {
+      values: PAYMENT_METHOD_VALUES,
+      message: `values other than ${PAYMENT_METHOD_VALUES.join(", ")} not supported`
+    },
+    required: true
   },
   user: {
     type: ObjectId,
-    ref: "User"
+    ref: "User",
+    required: true
   }
 }, { timestamps: true });
 
@@ -66,5 +90,9 @@ orderSchema.statics.calculateTotalPrice = function (order) {
 const Order = mongoose.model("Order", orderSchema);
 
 module.exports = {
-  Order, PAYMENT_METHOD, PAYMENT_STATUS, ORDER_STATUS
+  Order,
+  PAYMENT_METHOD, PAYMENT_METHOD_VALUES,
+  PAYMENT_STATUS, PAYMENT_STATUS_VALUES,
+  ORDER_STATUS, ORDER_STATUS_VALUES,
+  ORDER_PAGINATION_CONFIG
 }
--- routes/order.js
+++ routes/order.js
@@ -1,4 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const {
+  editOrder,
+  getAllOrders,
+  createOrder } = require("../controllers/order");
+const { isLoggedIn, isAdmin } = require("../middlewares/auth");
+const { getUserById } = require("../controllers/user");
+
+
+router.put("/create",
+  getUserById,
+  isLoggedIn,
+  createOrder
+);
+
+router.patch("/edit",
+  getUserById,
+  isLoggedIn,
+  isAdmin,
+  editOrder
+);
+
+router.get("/all",
+  getUserById,
+  isLoggedIn,
+  getAllOrders
+);
 
 module.exports = router;
